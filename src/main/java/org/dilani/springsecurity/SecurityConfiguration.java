a9310683f08201df491a4dc0113efafcc382040e
Dilani_Shyamali <we.shani92@gmail.com>
2025-02-07 07:38:38 +0530

    In Memory User Authentication changes

--- src/main/java/org/dilani/springsecurity/SecurityConfiguration.java
+++ src/main/java/org/dilani/springsecurity/SecurityConfiguration.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +25,32 @@ public class SecurityConfiguration {
                     registry.requestMatchers("/admin/**").hasRole("ADMIN");
                     registry.requestMatchers("/user/**").hasRole("USER");
                     registry.anyRequest().authenticated();
-                }).build();
+                })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+    }
+
+    //To the In memory user authentication
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.builder()
+                .username("gc")
+                .password("$2a$12$vZsFljUeqoFwcMSJI0H7Iutm2Qc0gJ3SMlD/Cvgd4nn8OXR0fYTya")
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("$2a$12$pYBk0H.Zv2UDEBd/FBfCkuEVnH.0c8FH/ijGkVXKzs2uRvJqJu2JK")
+                .roles("USER" , "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    //encoded password
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
